7776e959ef0f567f654029d7bfbc2c0ea3ab9a19
alex <laxnpander@gmail.com>
2020-11-27 15:38:46 +0100

    Initial commit

---
+++ include/simple.h
@@ -0,0 +1,8 @@
+//
+// Created by alex on 27.11.20.
+//
+
+#ifndef SIMPLE_PROJECT_SIMPLE_H
+#define SIMPLE_PROJECT_SIMPLE_H
+
+#endif //TRIAL1_SIMPLE_H
---
+++ src/simple.cpp
@@ -0,0 +1,4 @@
+//
+// Created by alex on 27.11.20.
+//
+

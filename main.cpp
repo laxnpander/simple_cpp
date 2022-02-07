4cbe91901af4fcd5564586e0573bec4ac197a7ae
Alex Kern <laxnpander@gmail.com>
2022-02-07 10:05:32 +0100

    Added formatting and tidy

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
-int main (int argc, char* argv[])
+int main ()
 {
-
-    return 0;
+  std::cout << "Hello world!" << std::endl;
+  bool test = true;
+  if (test || !test)
+  {
+    printf ("Huhu");
+  }
+  return 0;
 }

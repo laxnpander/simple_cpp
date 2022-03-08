336b56e7314854452956b6d01c10191c51b2406a
Alex Kern <laxnpander@gmail.com>
2022-03-08 11:49:48 +0100

    Updated structure

--- include/simple.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by alex on 27.11.20.
-//
-
-#ifndef SIMPLE_PROJECT_SIMPLE_H
-#define SIMPLE_PROJECT_SIMPLE_H
-
-#endif //TRIAL1_SIMPLE_H
--- src/simple.cpp
+++
@@ -1,4 +0,0 @@
-//
-// Created by alex on 27.11.20.
-//
-
